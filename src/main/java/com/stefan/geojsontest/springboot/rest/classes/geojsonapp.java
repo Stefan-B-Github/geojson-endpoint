f2eff43773b984ac39af5163dc49492f4707aaac
Stefan <stefan@razorsecure.com>
2024-06-14 21:31:42 +0100

    Initial commit to establish authentication server.

---
+++ src/main/java/com/stefan/geojsontest/springboot/rest/classes/geojsonapp.java
@@ -0,0 +1,13 @@
+package com.stefan.geojsontest.springboot.rest.classes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class geojsonapp {
+
+	public static void main(String[] args) {
+        SpringApplication.run(geojsonapp.class, args);
+	}
+
+}
---
+++ src/test/java/com/stefan/geojsontest/springboot/rest/classes/GeoJsonAppTests.java
@@ -0,0 +1,13 @@
+package com.stefan.geojsontest.springboot.rest.classes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoJsonAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
